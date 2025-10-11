ef7ccfeda75d37e083fb8665fdeac5ba7b53eb9b
DongChiMin <namv2704@gmail.com>
2025-10-11 22:48:45 +0700

    Update code SQP lấy danh sách bàn trống

--- src/java/dao/TableDAO.java
+++ src/java/dao/TableDAO.java
@@ -21,14 +21,25 @@ public class TableDAO {
     //Lấy danh sách các bàn trống trong khoảng thời gian bookingTime + 3 tiếng
     public List<Table> getAvailableTables(LocalDateTime bookingTime) throws SQLException {
         List<Table> tableList = new ArrayList<>();
-
+        
         String sql = """
-            SELECT * FROM tblTable
-                     
+            SELECT t.*
+            FROM tblTable t
+            WHERE t.id NOT IN (
+                SELECT rd.tblTableid
+                FROM tblReservationDetail rd
+                JOIN tblReservation r ON rd.tblReservationid = r.id
+                WHERE r.status = 'confirmed'
+                AND r.bookingTime >= DATE_SUB(?, INTERVAL 3 HOUR)
+            	AND r.bookingTime <= DATE_ADD(?, INTERVAL 3 HOUR)
+                )
+
                      """;
         try (Connection conn = DBUtil.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, bookingTime.toString());
+            ps.setString(2, bookingTime.toString());
             ResultSet rs = ps.executeQuery();
-
+            
             while (rs.next()) {
                 Table t = new Table();
                 t.setId(rs.getInt("id"));
